f8d3cf6620f502062b80501ec0c3021b0b4b0c61
jagomez <jgomez.career@gmail.com>
2015-01-07 11:59:18 -0600

    adding controller and routes for mentors

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#sample'
   get 'home' => 'pages#home'
 
+  resources :mentors
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
