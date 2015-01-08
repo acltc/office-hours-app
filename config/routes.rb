f8278d91bf5ed00d7f289b5989fb932ab2b077ea
Robert Elroy <relroy5499@gmail.com>
2015-01-08 16:17:08 -0600

    added API controllers, API routes, and JSON view file for mentors index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :mentors, only: [:index]
+      resources :categories, only: [:index]
+      resources :appointments, only: [:index, :create, :update, :show]
+    end
+  end
   root 'pages#sample'
   get 'home' => 'pages#home'
 
