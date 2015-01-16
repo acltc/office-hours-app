fe553a71ef6cde4b2f3121e42382c8af6b31bd5b
Robert Elroy <relroy5499@gmail.com>
2015-01-16 12:26:19 -0600

    added filter for angular show of appointments to mentor

--- app/assets/javascripts/api/v1/appointments_ctrl.js
+++ app/assets/javascripts/api/v1/appointments_ctrl.js
@@ -4,14 +4,12 @@
   angular.module("app").controller("appointmentsCtrl", function($scope, $http){
 
     $http.get("/api/v1/appointments.json").then(function (response) {
-      $scope.appointments = response.data;
+      $scope.appointments = response.data; 
     });
 
     $http.get("/api/v1/mentors.json").then(function (response) {
       $scope.mentors = response.data;
     });
-    
-    // $scope.testphrase = "Hello There"
 
     $scope.selectAppointment = function(appointment){
       $scope.greeting = " Your appointment! , " + (appointment.date_start_time) + " You should expect to spend " + (appointment.duration) + " minutes with your mentor!";
