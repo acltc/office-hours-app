e31e837639b6747b04d83708367a4dd7ae338ca9
Robert Elroy <relroy5499@gmail.com>
2015-01-16 17:10:29 -0600

    got appointments to go away after selection and only info to show for that appointment.

--- app/assets/javascripts/api/v1/appointments_ctrl.js
+++ app/assets/javascripts/api/v1/appointments_ctrl.js
@@ -13,8 +13,8 @@
 
     $scope.selectAppointment = function(appointment){
       $scope.greeting = " Your appointment! , " + (appointment.date_start_time) + " You should expect to spend " + (appointment.duration) + " minutes with your mentor!";
-      // appointment.available = !appointment.available;
-      // $scope.appointments = null;
+      appointment.available = !appointment.available;
+      $scope.appointments = null;
     };
       window.scope = $scope;
     });
