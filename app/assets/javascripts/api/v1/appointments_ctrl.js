30b42da718fb16986bcfc5ec468c0547bcb7c337
Robert Elroy <relroy5499@gmail.com>
2015-01-17 16:36:37 -0600

    changed view to Ruby for appointments and fixed controller too.

--- app/assets/javascripts/api/v1/appointments_ctrl.js
+++ app/assets/javascripts/api/v1/appointments_ctrl.js
@@ -12,7 +12,7 @@
     });
 
     $scope.selectAppointment = function(appointment){
-      $scope.greeting = " Your appointment! , " + (appointment.date_start_time) + " You should expect to spend " + (appointment.duration) + " minutes with your mentor!";
+      $scope.greeting = " Your appointment! , " + (appointment.date_start_time ) + " You should expect to spend " + (appointment.duration) + " minutes with your mentor!";
       appointment.available = !appointment.available;
       $scope.appointments = null;
     };
