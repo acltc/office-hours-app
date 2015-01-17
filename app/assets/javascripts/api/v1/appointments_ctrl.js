a98610c3416d21fd3e40799eb63082503148f7bb
walshification <walshification@gmail.com>
2015-01-17 17:43:18 -0600

    created angular form for new appointments (not complete)

--- app/assets/javascripts/api/v1/appointments_ctrl.js
+++ app/assets/javascripts/api/v1/appointments_ctrl.js
@@ -7,6 +7,23 @@
       $scope.appointments = response.data;
     });
 
+    $scope.createAppointment = function(newDateStartTime, newDuration, newMentorId) {
+      var newAppointment = {
+        date_start_time: newDateStartTime, 
+        duration: newDuration, 
+        mentor_id: newMentorId
+      };
+      $http.post("/api/v1/appointments.json", {appointment: newAppointment}).then(function (response) {
+
+        }, function (error) {
+          $scope.error = error.statusText;
+        });
+
+      $scope.appointments.push(newAppointment);
+      $scope.newDateStartTime = "";
+      $scope.newDuration = "";
+    };
+
     $scope.selectAppointment = function(appointment){
       $scope.greeting = "Your drone, " + (appointment.date_start_time) + " will be on their way! You should expect your Ice cream within 30 minutes!";
       appointment.available = !appointment.available;
