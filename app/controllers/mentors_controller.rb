bff5566e05466adfbf7b5d5401cdf18ad4084412
“kylemullengit config --global user.email “kylem.mullen.com <kylem.mullen@gmail.com>
2015-01-18 19:01:08 -0600

    Finished the new and create, edit and update, and destroy actions for Mentors along with their forms

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,4 +1,5 @@
 class MentorsController < ApplicationController
+	before_action :authenticate_admin!, :only => [:edit, :destroy, :new, :create]
 
 	def index
 		@mentors = Mentor.all
@@ -15,21 +16,46 @@ class MentorsController < ApplicationController
 	end
 
 	def new
+		@mentor = Mentor.new
+		@categories = Category.all 
 	end
 
 	def create
+		@mentor = Mentor.create(mentor_params)
+		redirect_to '/mentors'
 	end
 
 	def edit
+		@categories = Category.all
+		@mentor = Mentor.find_by(:id => params[:id])
 	end
 
 	def update
-	end
-
-	def delete
+		@categories = Category.all
+		@mentor = Mentor.find_by(:id => params[:id])
+		@mentor.update(mentor_params)
+		flash[:info] = "Mentor Succesfully Modified."
+		redirect_to @mentor
 	end
 
 	def destroy
+		@mentor = Mentor.find_by(:id => params[:id])
+		@mentor.destroy
+		flash[:info] = "Mentor Succesfully Deleted."
+		redirect_to '/mentors'
+	end
+
+	private 
+
+	def authenticate_admin!
+		unless user_signed_in? && current_user.admin?
+				
+		end
+	end
+
+	def mentor_params
+		return params.require(:mentor).permit(:first_name, :last_name, :email, :bio, :photo, :category_id, :commit, :id)
+
 	end
 
 end
