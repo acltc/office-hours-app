b8c35d5714af35b4e1b779638fb3851d3df15578
Robert Elroy <relroy5499@gmail.com>
2015-01-19 15:20:38 -0600

    trying out new things on angular, time filter, name and email fields.

--- app/assets/javascripts/appointments_ctrl.js
+++ app/assets/javascripts/appointments_ctrl.js
@@ -12,7 +12,12 @@
     });
 
     $scope.selectAppointment = function(appointment){
-      $scope.greeting = " Your appointment! , " + (appointment.date_start_time ) + " You should expect to spend " + (appointment.duration) + " minutes with your mentor!";
+      $scope.greeting =  " You are now scheduled. You should expect to spend " + (appointment.duration) + " minutes with your mentor!";
+      // var updatedAppointment = { mentee_name: appointment.mentee_name, mentee_email: appointment.mentee_email };
+      // $http.patch('/api/v1/appointments/' + $scope.currentAppointmentId + '.json', {appointment: updatedAppointment}).then(function(response) {
+      //     $scope.currentAppointment.mentee_name = appointment.mentee_name;
+      //   }, function (error) {
+      //     $scope.error = error.statusText;
       appointment.available = !appointment.available;
       $scope.appointments = null;
     };
