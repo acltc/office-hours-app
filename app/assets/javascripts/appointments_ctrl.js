203c25c4a3f1713ce020330b9fe8f304d33e6158
Christopher Walsh <walshification@gmail.com>
2015-02-05 08:09:16 -0600

    hotfixing the appointments_ctrl

--- app/assets/javascripts/appointments_ctrl.js
+++ app/assets/javascripts/appointments_ctrl.js
@@ -44,26 +44,7 @@
 
     $scope.selectAppointment = function(appointment){
       console.log(appointment);
-      <<<<<<< HEAD
-      alert("You will be scheduled for " + (appointment.duration) + " minutes with this mentor. Please enter your first and last name and your email address in the appropriate section and submit your request.");
-        // $scope.greeting =  " You are now scheduled. You should expect to spend " + (appointment.duration) + " minutes with your mentor! "; 
-        
-        $scope.appointments = null;
-
-        $scope.enterInfo = function(addName, addEmail){
-          alert("Thank you! You're appointment is scheduled. If you need to cancel please contact 1871");
-          // $scope.greetingTwo = "Thank you! ";
-          $scope.addName = "";
-          $scope.addEmail = "";
-          console.log(addName, addEmail);
-          var updatedAppointment = { mentee_name: (addName), mentee_email: (addEmail), available: false };
-          $http.patch('/api/v1/appointments/' + (appointment.id) + '.json', {appointment: (updatedAppointment)}).then(function(response) {
-
-          }, function (error) {
-            $scope.error = error.statusText;
-          });
-        };
-        =======
+      
       // alert("You will be scheduled for " + (appointment.duration) + " minutes with this mentor. Please enter your first and last name and your email address in the appropriate section and submit your request.");
       $scope.greeting =  "You will be scheduled for " + (appointment.duration) + " minutes with this mentor. Please enter your first and last name and your email address in the section below and submit your request."; 
       $scope.appointments = null;
@@ -80,9 +61,10 @@
       }, function (error) {
         $scope.error = error.statusText;
       });
-        >>>>>>> 71bd922e3f3be8ce33dbef971e25f314976c7ffb
       };
-      window.scope = $scope;
-    });
+    };
+
+    window.scope = $scope;
+  });
 
 }());
