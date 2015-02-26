2e9d5309f9639f9aa3c71bfabcf4174871519133
Robert Elroy <relroy5499@gmail.com>
2015-02-26 11:09:56 -0600

    corrected email address for 1871

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,12 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 
-# ActionMailer::Base.smtp_settings = {
-#   :user_name => ENV["USER_NAME"],
-#   :password => ENV["PASSWORD"],
-#   :domain => ENV["DOMAIN"],
-#   :address => ENV["ADDRESS"],
-#   :port => 587,
-#   :authentication => :plain,
-#   :enable_starttls_auto => true
-# }
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'relroy5499@gmail.com',
+  :password => 'Smitty10',
+  :domain => 'example.com',
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
